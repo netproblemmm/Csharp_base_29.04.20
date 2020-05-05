e7fc8596b64e7e7cc0ded9081a55264b111e9ec8
netproblemmm <ubhawk1@mail.ru>
2020-05-06 02:39:31 +0300

    Выполнил 1-5 задания

---
+++ Lesson2_task3_SumOfOddPositive/Lesson2_task3_SumOfOddPositive.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+// Ястребов Юрий
+// 3. С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+class Lesson2_task3_SumOfOddPositive
+{
+    static void Main()
+    {
+        View view = new View();
+        int digitQty = DigitQty(view);
+        view.Print(digitQty);
+        view.Pause();
+    }
+
+    private static int DigitQty(View view)
+    {
+        int number;
+        int sum = 0;
+        do
+        {
+            view.Print("Введите число");
+            number = view.GetInt();
+            if (IsDigitOdd(number) && IsDigitPositive(number))
+                sum += number;
+        }
+        while(number != 0);
+
+        return sum;
+    }
+
+    private static bool IsDigitOdd(int digit)
+    {
+        if (digit % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    private static bool IsDigitPositive(int digit)
+    {
+        if (digit > 0)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ Lesson2_task4_LoginAndPassword/View.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class View
+{
+    public void Print(object value, bool isNewLine = true)
+    {
+        if (isNewLine)
+        {
+            Console.WriteLine(value);
+        }
+        else
+        {
+            Console.Write(value);
+        }
+    }
+    
+    public void Pause()
+    {
+        Console.ReadKey();
+    }
+
+    public string GetString()
+    {
+        return Console.ReadLine();
+    }
+
+    public int GetInt()
+    {
+        return Int32.Parse(GetString());
+    }
+
+    public double GetDouble()
+    {
+        return Double.Parse(GetString().Replace('.', ','));
+    }
+}
